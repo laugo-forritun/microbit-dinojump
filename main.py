4a3a6603e4c79dc2f50f0207f57e68ecb613a1ac
laugo-forritun <77778082+laugo-forritun@users.noreply.github.com>
2022-01-31 12:27:00 +0000

    update

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 FPS = 30
 dinosaur_y = 0
-y_acceleration = 0.03
+y_acceleration = 0.1
 y_speed = 0
-JUMP_FORCE = 40
+JUMP_FORCE = 0.8
+
 
 def touching_ground():
     return dinosaur_y >= 4
 
+
 def fall():
     global dinosaur_y, y_speed
+    # Falling in air
+    y_speed += y_acceleration
+    dinosaur_y += y_speed
+
+    # Reset if below/on ground
     if touching_ground():
         # Stand on ground
         y_speed = 0
         dinosaur_y = 4
-    else:
-        # Falling in air
-        y_speed += y_acceleration
-        dinosaur_y += y_speed
+
 
 def jump():
     global y_speed
@@ -24,13 +28,17 @@ def jump():
         # Only jump if on ground
         y_speed -= JUMP_FORCE
 
+
 def draw():
     basic.clear_screen()
     led.plot(1, dinosaur_y)
-    basic.pause(1000/FPS)
+    basic.pause(1000 / FPS)
+
 
 def on_forever():
     fall()
     jump()
     draw()
+
+
 basic.forever(on_forever)
